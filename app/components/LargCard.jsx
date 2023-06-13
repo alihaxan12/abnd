a3b5dec67c62fd9e087c1ccb3ac5e4ad92a74b24
Ali Hassan <alihaxan514@gmail.com>
2023-06-13 18:12:27 +0500

    update left again

--- app/components/LargCard.jsx
+++ app/components/LargCard.jsx
@@ -13,7 +13,7 @@ export const LargCard = () => {
                 className='rounded-xl'
                 />
             </div>
-            <div className='absolute top-32 left-12 md:left-32'>
+            <div className='absolute top-32 left-12 md:left-64'>
                 <h3 className='text-4xl mb-3 w-64 font-semibold'>The Greatest Outdoors</h3>
                 <p>Whishlist Created by Airbnd</p>
                 <button className='text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5'>
